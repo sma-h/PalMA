0b102d8727318ff001b1331daf9b1fde8d94b816
Jörg Mechnich <joerg.mechnich@bib.uni-mannheim.de>
2019-11-13 17:21:32 +0100

    renamed handler for web pages from 'midori' to 'palma-browser'

--- index.php
+++ index.php
@@ -352,20 +352,20 @@ function getHandlerCommand(handle, task) {
     handler["default"]["clockwise"] = "undefined";
 
     // Handler for web pages.
-    handler["midori"] = {};
-    handler["midori"]["up"] = "Up";
-    handler["midori"]["down"] = "Down";
-    handler["midori"]["left"] = "Left";
-    handler["midori"]["right"] = "Right";
-    handler["midori"]["next"] = "Next";
-    handler["midori"]["prior"] = "Prior";
-    handler["midori"]["home"] = "Home";
-    handler["midori"]["end"] = "End";
-    handler["midori"]["zoomin"] = "ctrl+plus";
-    handler["midori"]["zoomout"] = "ctrl+minus";
-    handler["midori"]["download"] = "undefined";
-    handler["midori"]["counterclockwise"] = "undefined";
-    handler["midori"]["clockwise"] = "undefined";
+    handler["palma-browser"] = {};
+    handler["palma-browser"]["up"] = "Up";
+    handler["palma-browser"]["down"] = "Down";
+    handler["palma-browser"]["left"] = "Left";
+    handler["palma-browser"]["right"] = "Right";
+    handler["palma-browser"]["next"] = "Next";
+    handler["palma-browser"]["prior"] = "Prior";
+    handler["palma-browser"]["home"] = "Home";
+    handler["palma-browser"]["end"] = "End";
+    handler["palma-browser"]["zoomin"] = "ctrl+plus";
+    handler["palma-browser"]["zoomout"] = "ctrl+minus";
+    handler["palma-browser"]["download"] = "undefined";
+    handler["palma-browser"]["counterclockwise"] = "undefined";
+    handler["palma-browser"]["clockwise"] = "undefined";
 
     // Handler for images.
     handler["feh"] = {};
@@ -700,7 +700,9 @@ function addWindowControls(layout, controls, screensection, file, status) {
     topbar.appendChild(keyControl(screensection, 'fa fa-rotate-left', 'counterclockwise', 'counterclockwise', handler, !counterclockwise, '<?=__("Rotate counterclockwise")?>'));
     topbar.appendChild(keyControl(screensection, 'fa fa-rotate-right', 'clockwise', 'clockwise', handler, !clockwise, '<?=__("Rotate clockwise")?>'));
     var downloadbutton = keyControl(screensection, 'fa fa-download', 'download', 'download', handler, !download, '<?=__("Download this item")?>');
-    downloadbutton.setAttribute('onclick', 'downloadFile(' + screensection + ')');
+    if(download) {
+      downloadbutton.setAttribute('onclick', 'downloadFile(' + screensection + ')');
+    }
     topbar.appendChild(downloadbutton);
     button = document.createElement('button');
     button.setAttribute("class", "trash");
@@ -788,7 +790,7 @@ function updateWindowList(window){
             button.setAttribute("class", "window_entry_button");
             button.setAttribute('onclick', "openAccordion('" + divID + "')");
             var icon = document.createElement('i');
-            if (handler.indexOf("midori") > -1) {
+            if (handler.indexOf("palma-browser") > -1) {
                 icon.setAttribute("class", "fa fa-globe");
             } else if (handler.indexOf("vnc") > -1) {
                 icon.setAttribute("class", "fa fa-video-camera");
@@ -858,8 +860,8 @@ function updateControlsBySection(window) {
                 if (handler.indexOf("--writer") > -1) {
                     control = ["libreoffice-writer", true, true, true, true, false, false, false, false, true, true, true, false, false];
                 }
-        } else if (handler.indexOf("midori") > -1) {
-            control = ["midori", true, true, true, true, true, true, true, true, true, true, false, false, false];
+        } else if (handler.indexOf("palma-browser") > -1) {
+            control = ["palma-browser", true, true, true, true, true, true, true, true, true, true, false, false, false];
         } else if (handler.indexOf("vlc") > -1) {
             control = ["vlc", false, false, false, true, false, false, false, false, false, false, false, false, false];
         } else if (handler.indexOf("vnc") > -1) {
